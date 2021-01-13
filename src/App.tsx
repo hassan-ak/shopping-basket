51e6ab7cfff885bc9356b315a28a2711c18da8cc
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-13 13:48:36 +0500

    Create Home component
    a:- Created
    	i:- src\components\Home.tsx to define Home component
    	ii:- Home image in asserts folder
    b:- Updated
    	i:- src\App.tsx to add Home component in the app
    	ii:- src\App.css to define styling for Home component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@
 import React from 'react';
 // Component Imports
 import Header from './components/Header';
+import { Home } from './components/Home';
 // Styles Imports
 import './App.css';
-
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className="">
+        <Home/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,61 @@
+// Imports
+// React Imports
+import React from 'react'
+// Material Ui Imports
+import { Button, Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+// Images Imports
+import home from '../asserts/images/home.png';
+
+// Styling for home Component
+const useStyles = makeStyles((theme) => ({
+    mainGrid: {
+        display:'flex',
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor:'#1D2521',
+    },
+    componentGrid: {
+        backgroundColor:'#1D2521',
+        padding:'20px',
+        border:'0'
+    },
+    startButton: {
+        backgroundColor: '#298155',
+        textDecoration: "none",
+        width: "100%" 
+    },
+}));
+
+// Home Component
+export const Home = () => {
+    //  useStyles
+    const classes = useStyles();
+    return (
+        <div className="homeContainer">
+            <Grid container className={classes.mainGrid}>
+                    <Grid item xs={8} md={5} component={Card} className={classes.componentGrid} elevation={0}> 
+                        <CardContent>
+                            <img src={home} className="homeImage" alt="shoeStore"/>
+                        </CardContent>
+                    </Grid>
+                    <Grid item xs={8} md={5} component={Card} className={classes.componentGrid} elevation={0}> 
+                        <CardContent className="homeContent">
+                            <Typography variant= "h2" align = "center" gutterBottom className="title">
+                                Shoe Store
+                            </Typography>
+                            <Typography variant= "h6" gutterBottom className="homeDetail" >
+                                Finest Shoes you can find online.
+                            </Typography>
+                            <Button
+                                variant="contained"
+                                className={classes.startButton}
+                            >
+                                Store
+                            </Button>
+                        </CardContent>
+                    </Grid>
+            </Grid>
+        </div>
+    )
+} 
